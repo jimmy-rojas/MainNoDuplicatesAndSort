8d7d6a4f7f3a45e4a8c00940b7a66cc046b092f4
Jimmy Rojas <jimmy.rojas@truextend.com>
2021-11-30 17:48:42 -0400

    removed unnecessary comments and include main util calls

--- src/test/java/org/jorm/sample/UtilDuplicatesTest.java
+++ src/test/java/org/jorm/sample/UtilDuplicatesTest.java
@@ -8,8 +8,6 @@ public class UtilDuplicatesTest {
     @Test
     public void test_removeDuplicates() {
         String[] input = new String[]{"A-","A","A-","B+","A","C-","D","B+","B-","A","F","A+","C+","F","D-","F","B+","A"};
-//        String[] input = new String[]{"A-","A","A-"};
-//        String[] output = new String[]{"A-","A"};
         String[] output = new String[]{"A-","A","B+","C-","D","B-","F","A+","C+","D-"};
         Assert.assertEquals(output, UtilDuplicates.removeDuplicates(input));
     }
