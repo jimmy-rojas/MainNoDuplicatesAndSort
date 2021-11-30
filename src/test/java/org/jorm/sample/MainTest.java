29e5f370b0040302c8bcddb65889e2ae4f20b604
Jimmy Rojas <jimmy.rojas@truextend.com>
2021-11-30 16:13:12 -0400

    prepare TDD env

---
+++ src/test/java/org/jorm/sample/MainTest.java
@@ -0,0 +1,17 @@
+package org.jorm.sample;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MainTest {
+
+    @Before
+    public void setup() {
+    }
+
+    @Test
+    public void test_put() {
+        Assert.assertTrue(true);
+    }
+}
